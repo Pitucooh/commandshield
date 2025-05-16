16eeb2264b09252a5caf5794246bf5e0d90eb1ec
Verina Wadie <verinawadie1@gmail.com>
2025-05-16 08:55:55 -0300

    update banco

--- CommandShield/gerenciamento_usuarios.h
+++ CommandShield/gerenciamento_usuarios.h
@@ -1,21 +1,16 @@
-// gerenciamento_usuarios.h
 #ifndef GERENCIAMENTO_USUARIOS_H
 #define GERENCIAMENTO_USUARIOS_H
 
-// Estrutura de usuário
+#include <mysql/mysql.h>
+
 typedef struct {
-    char username[50];
-    char password_hash[100];
+    char username[100];
     int is_admin;
 } User;
 
-// Autentica um usuário, retorna 1 se autenticado, 0 caso contrário
-int authenticate_user();
-
-// Verifica se o usuário atual tem permissão de administrador
-int is_admin_user();
-
-// Variável que deve ser acessível externamente
 extern User current_user;
+
+int authenticate_user();
+int is_admin_user();
 
 #endif
